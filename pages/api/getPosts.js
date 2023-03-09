fac6ff39ae4d0bed8e8fca7debfbaabba6cd10b1
Abdul1rhman <pain008900@gmail.com>
2023-03-09 10:55:25 +0300

    fix bug

--- pages/api/getPosts.js
+++ pages/api/getPosts.js
@@ -5,7 +5,7 @@ export default async  (req, res) => {
     const client = await clientPromise;
     const db = client.db("posts");
 
-    const posts = await db.collection("posts").find({}).limit(20).toArray();
+    const posts = await db.collection("posts").find({}).sort({_id:1}).limit(20).toArray();
 
     res.json(posts);
   } catch (e) {
