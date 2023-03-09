9e102f2e46f763f472b2bd8cbbfcb05911b395d8
Abdul1rhman <pain008900@gmail.com>
2023-03-09 11:20:21 +0300

    fix the time

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,7 @@ const [error, setError]=useState('')
                 
                 </div>
                 <Card.Footer className="text-muted mt-4 time">
-                date: {item.today}- hour: {item.hour} 
+                date: {item.today}- hour: {item.hour.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })} 
                 <div style={{ color:'black',display:'flex', justifyContent:'end',alignItems:'center', gap:'10px',fontSize:'20px'}}>
                 <Link className="" style={{textDecoration:'none', color:'black',display:'flex', justifyContent:'end',alignItems:'center'}} href={`/posts/${item._id}`}><FaRegEdit/></Link>
                 <div style={{cursor:'pointer',display:'flex', justifyContent:'end',alignItems:'center',color:'red'}} className="  " onClick={e=>{handelDelete(item._id)}}><AiOutlineDelete/></div>
