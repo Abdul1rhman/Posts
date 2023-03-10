9b4341b13d9922bd501c13c631cb4ed01e530e2c
Abdul1rhman <pain008900@gmail.com>
2023-03-11 01:05:33 +0300

    fix server

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,7 +15,7 @@ export const authOptions = {
       })
     // ...add more providers here
   ],
-  secret: process.env.jwt_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 
 }
 export default NextAuth(authOptions)
