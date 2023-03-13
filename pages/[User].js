9b993c94ceeb0ab2a96c0b03372a91da119f7387
Abdul1rhman <pain008900@gmail.com>
2023-03-13 03:07:09 +0300

    fix style

--- pages/[User].js
+++ pages/[User].js
@@ -80,12 +80,14 @@ const [error, setError]=useState('')
                 </div>
                 <Card.Footer className="text-muted mt-4 time">
                 date: {item.today}- hour: {item.hour} 
-                <div style={{ color:'black',display:'flex', justifyContent:'end',alignItems:'center', gap:'10px',fontSize:'20px'}}>
+                
+                
+                </Card.Footer>  
+
+                <div className='time-icon' style={{ color:'black',display:'flex', justifyContent:'end',alignItems:'center', gap:'10px',fontSize:'20px'}}>
                 <Link className="" style={{textDecoration:'none', color:'black',display:'flex', justifyContent:'end',alignItems:'center'}} href={`/posts/${item._id}`}><FaRegEdit/></Link>
                 <div style={{cursor:'pointer',display:'flex', justifyContent:'end',alignItems:'center',color:'red'}} className="  " onClick={e=>{handelDelete(item._id)}}><AiOutlineDelete/></div>
                 </div>
-                
-                </Card.Footer>  
                 
 
             </div>
